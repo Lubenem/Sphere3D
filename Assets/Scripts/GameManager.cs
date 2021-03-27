9d0ef19705e04c4e63a14dfbb7a4bfd932bf33b9
Liubomyr <Lubenemkrkm@gmail.com>
2021-03-27 18:37:42 +0200

    Obstacle generator

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -8,10 +10,15 @@ public class GameManager : MonoBehaviour
     [SerializeField] private AudioClip _completeGameSound;
     [SerializeField] private AudioClip _failGameSound;
     [SerializeField] private UnityEvent _onComplete;
+    [SerializeField] private int enemyCount = 35;
+    [SerializeField] private Obstacle _obstaclePrefab;
+    [SerializeField] private Transform _obstacleParent;
 
     public static GameManager instance;
     public Path path;
     public PlayerSphere playerSphere;
+
+    public List<Obstacle> obstacles;
 
     private void Awake()
     {
@@ -21,6 +28,26 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
 
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        GenerateObstacles();
+    }
+
+    private void GenerateObstacles()
+    {
+        obstacles = new List<Obstacle>();
+
+        for (int i = 0; i < enemyCount; i++)
+        {
+            float xPos = Random.Range(-1.5f, 1.5f);
+            float zPos = Random.Range(-1.7f, 2.3f);
+
+            Obstacle obstacle = Instantiate(_obstaclePrefab, _obstacleParent);
+            obstacle.transform.localPosition = new Vector3(xPos, transform.localPosition.y, zPos);
+            obstacles.Add(obstacle);
+        }
     }
 
     public void CompleteGame()
--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -3,18 +3,16 @@ using UnityEngine;
 public class Path : MonoBehaviour
 {
     private Collider _collider;
-    private Obstacle[] _obstacles;
 
     private void Start()
     {
-        _obstacles = FindObjectsOfType<Obstacle>();
         _collider = GetComponent<Collider>();
         GameManager.instance.path = this;
     }
 
     public void PathCheck()
     {
-        foreach (var obstacle in _obstacles)
+        foreach (Obstacle obstacle in GameManager.instance.obstacles)
         {
             Collider obstacleCollider = obstacle.obstacleCollider;
             if (_collider.bounds.Intersects(obstacleCollider.bounds))
